8e0a0f3115482f0ef1c807570b78be7411b6ed97
mrxxnal <mrunal.b2001@gmail.com>
2026-04-26 21:30:51 -0600

    fix streamlit image compatibility AGAIN

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ uploaded_file = st.file_uploader("Upload a toy image", type=["jpg", "png", "jpeg
 if uploaded_file is not None:
 
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(image, caption="Uploaded Image")
 
     results = find_similar(image, df, index)
 
